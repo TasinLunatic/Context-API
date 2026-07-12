be8bba8c86aba8d14c3342b563ca0e211b3c0381
TasinLunatic <ahmedtasin207@gmail.com>
2026-07-12 21:00:30 +0800

    404 not fond page fixed and WOP

--- react-router/src/router/AppRouter.jsx
+++ react-router/src/router/AppRouter.jsx
@@ -8,19 +8,20 @@ import Dashboard from "../pages/Dashboard";
 import Profile from "../pages/Profile";
 
 export default function AppRouter() {
-    return (<BrowserRouter>
-     <Routes>
-      <Route path="/" element={<RootLayout />}> 
-        <Route index element={<Dashboard/>} />
-        <Route path="dashboard" element={<Dashboard/>} />
-        <Route path=":profileID" element={<Profile/>} />
-        <Route path="settings" element={<h1>settings</h1>} />
-        <Route path="help" element={<h1>Help</h1>} />
-      </Route>
-      <Route path="/login" element={<Login />} />
-      <Route path="/about" element={<About/>} />
-      <Route path="*" element={<NotFound/>} />
-    </Routes>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="profile/:profileID" element={<Profile />} />
+          <Route path="settings" element={<h1>settings</h1>} />
+          <Route path="help" element={<h1>Help</h1>} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
-    );
-};
+  );
+}
