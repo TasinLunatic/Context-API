77881228548dcca33eee2a5d63d6c1e6027006cd
TasinLunatic <ahmedtasin207@gmail.com>
2026-07-12 21:23:31 +0800

    fix all css issues and make the them button exact like header

--- react-router/src/pages/RootLayout.jsx
+++ react-router/src/pages/RootLayout.jsx
@@ -1,21 +1,16 @@
-import React from 'react'
-import Header from '../components/Header';
-import MainContent from '../components/MainContent';
-import Sidebar from '../components/Sidebar';
-import ThemeProvider from "../provider/ThemeProvider";
-import { Outlet } from 'react-router';
+import React from "react";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import { Outlet } from "react-router";
 
 export default function RootLayout() {
   return (
-   <ThemeProvider> 
-    <div className='min-h-screen flex flex-col'>
-      <Header/>
-      <div className='flex flex-1'>
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <div className="flex flex-1">
         <Sidebar />
         <Outlet />
-        {/* <MainContent /> */}
-      </div>     
+      </div>
     </div>
-    </ThemeProvider>
   );
 }
