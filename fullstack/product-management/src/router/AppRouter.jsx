3b0a4b997ae2f50b6dbdc0ac4ea25f27d393a9f2
TasinLunatic <ahmedtasin207@gmail.com>
2026-07-31 22:01:23 +0800

    added all pages from context-api and added authcontext and approuter

---
+++ fullstack/product-management/src/router/AppRouter.jsx
@@ -0,0 +1,40 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import About from "../pages/About";
+import Login from "../pages/Login";
+import NotFound from "../pages/NotFound";
+import RootLayout from "../pages/RootLayout";
+import Dashboard from "../pages/Dashboard";
+import Profile from "../pages/Profile";
+import ThemeProvider from "../provider/ThemeProvider";
+import Settings from "../pages/Settings";
+import Help from "../pages/Help";
+import { AuthProvider, useAuth } from "../contexts/AuthContext";
+
+function ProtectedProfile() {
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated ? <Profile /> : <Navigate to="/login" replace />;
+}
+
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <ThemeProvider>
+          <Routes>
+            <Route path="/" element={<RootLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="profile" element={<ProtectedProfile />} />
+              <Route path="profile/:profileID" element={<ProtectedProfile />} />
+              <Route path="about" element={<About />} />
+              <Route path="settings" element={<Settings />} />
+              <Route path="help" element={<Help />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </ThemeProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
