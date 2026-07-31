890096d3a882c59527eefa228a39c49ef99e9e9b
TasinLunatic <ahmedtasin207@gmail.com>
2026-07-31 21:50:49 +0800

    set the vite and copy some parts from Context-Api ,also WOP

---
+++ fullstack/product-management/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import clsx from "clsx";
+import useTheme from "../hooks/useTheme";
+
+
+export default function Header( ) {
+    const {theme, toggleTheme} = useTheme();
+  return (
+    <header className={clsx('p-4 border-b-2 transition-colors duration-300',
+            theme === "light" && "bg-slate-200 border-slate-200 text-slate-800",
+            theme === "dark" && "bg-slate-900 border-slate-600 text-slate-200"
+        )}>
+
+       <div className="flex justify-between items-center max-w-4xl mx-auto">
+        <h1 className="text-2xl font-bold">My App</h1>
+        <div className="flex items-center gap-4">
+            <span className="">Welcome, Guest!</span>
+            <button onClick={toggleTheme} className={clsx('px-4 py-2 rounded-lg font-medium cursor-pointer',
+                theme === "light" && "bg-gray-800 text-white hover:bg-gray-700" ,
+                theme=== "dark" && "bg-white text-gray-800 hover:bg-gray-100")}>
+                {theme === "light" ? "🌙 Dark" : "☀️ Light"}
+            </button>
+        </div>
+       </div>
+    </header>
+      
+  )
+}
