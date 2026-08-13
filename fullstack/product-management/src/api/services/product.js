4e426be54398ba1df85f22a8d85d4f48629a7c54
TasinLunatic <ahmedtasin207@gmail.com>
2026-08-14 00:19:55 +0800

    useState , callback fuction use for save time and render the products

--- fullstack/product-management/src/api/services/product.js
+++ fullstack/product-management/src/api/services/product.js
@@ -2,6 +2,10 @@ import axios from "axios";
 
 export const getProducts = async () => {
   try {
-    const response = await axios.get("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"); 
+    const response = await axios.get("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json");
     return response.data;
-  };
+  }catch (error) {
+    console.error("Failed to fetch products:", error);
+    return [];
+  }
+}
--- fullstack/product-management/src/pages/ProductManagement.jsx
+++ fullstack/product-management/src/pages/ProductManagement.jsx
@@ -1,11 +1,14 @@
-import {useCallback, useEffect} from 'react'
+import {useCallback, useEffect, useState} from 'react'
 import axios from 'axios'
 import { productService } from '../api/services'
 
 export default function ProductManagement() {
+   const [products, setProducts] = useState([]);
+
    const getProducts =useCallback (async () => {
       try {
          const products = await productService.getProducts();
+         setProducts(products);
       } catch (err) {
          console.log(err);
          alert('Error fetching data');
@@ -16,7 +19,16 @@ export default function ProductManagement() {
       getProducts();
     }, [getProducts]);
 
-  return<div>ProductManagement</div>
+  return(
+   <div className="p-4">
+      <h1 className="text-2xl font-bold">Product Management</h1>
+      <div className='grid grid-cols-3 gap-4 mt-4'>
+         {products.map((product) => (
+            <div key={product.name}>{product.name}</div>
+         ))}
+      </div>
+   </div>
+  );
   
 }
 
