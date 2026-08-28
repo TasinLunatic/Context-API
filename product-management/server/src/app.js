8d5abf2dcb23908b11c011b9060b7bb120732b3c
TasinLunatic <ahmedtasin207@gmail.com>
2026-08-29 01:03:49 +0800

    added a API and set data and connect it with client

--- product-management/server/src/app.js
+++ product-management/server/src/app.js
@@ -1,10 +1,25 @@
 import express from 'express';
+import cors from 'cors';
+import {mockProducts} from './mockdata.js'
 const app = express()
+
+app.use(express.json())
+app.use(cors({
+    origin : 'http://localhost:3009',
+})
+);
+    
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/status', (req, res) => {
+  res.json({
+    message:'Hello World!'
+   })
 })
+
+app.get('/api/products', (req, res) => {
+  res.status(200).json(mockProducts)
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
