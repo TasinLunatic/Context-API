b4e8e9c7a18ccda7dac33f895948c7f52a3f5f54
TasinLunatic <ahmedtasin207@gmail.com>
2026-08-29 23:38:32 +0800

    dotenv library added and the configure setup done

--- product-management/server/src/config/envConfig.js
+++ product-management/server/src/config/envConfig.js
@@ -1,6 +1,10 @@
+import dotenv from 'dotenv'
+dotenv.config();
+
+
 const envConfig = {
-    PORT: 5000,
-    ALLOWED_ORIGINS: 'http://localhost:3009',
+    PORT: process.env.PORT || 5000,
+    ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS || 'http://localhost:3009',
 };
 
 export default envConfig;
