a92acda156067aeb0d07f6601015dea322d7373e
TasinLunatic <ahmedtasin207@gmail.com>
2026-08-31 02:54:09 +0800

    added request and response with emoji in logger

--- product-management/server/src/middlewares/logger.js
+++ product-management/server/src/middlewares/logger.js
@@ -1,15 +1,17 @@
 const logger = (req, res, next) => {
-  const reqURL=req.url;
+  const reqURL = req.url;
   const startTime = Date.now();
 
   res.on("finish", () => {
     const duration = Date.now() - startTime;
     console.log(
-      `[${new Date().toISOString()}]:  ${req.method} ~ ${reqURL} ~ ${res.statusCode} - ${duration}ms`,
+      `[${new Date().toISOString()}]: ✅ RESPONSE ${req.method} ~ ${reqURL} ~ ${res.statusCode} - ${duration}ms`,
     );
   });
 
-  console.log(`[${new Date().toISOString()}]:  ${req.method} ~ ${reqURL}`);
+  console.log(
+    `[${new Date().toISOString()}]: 📡 REQUEST ${req.method} ~ ${reqURL}`,
+  );
   next();
 };
 
